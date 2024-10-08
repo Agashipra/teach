88ae673fadd3056a8ee2a9cb14ab0a9d77cb0ba7
Udit Agrawal <143325564+Udit-exe@users.noreply.github.com>
2024-10-08 12:57:06 +0000

    modified:   www.teachbuddies.com/index.html 	modified:   www.teachbuddies.com/js/paymentDetails.js

--- www.teachbuddies.com/js/paymentDetails.js
+++ www.teachbuddies.com/js/paymentDetails.js
@@ -1,16 +1,17 @@
-const scriptURL = ''
+const scriptURL = '' // Add your script URL here
 
 const form = document.forms['paymentForm']
 
-form.addEventListener('submit' , e =>{
+form.addEventListener('submit', e => {
     e.preventDefault()
-    fetch(scriptURL, {method: 'POST', body: new FormData(form)})
-    .then(response =>  {
-        alert("Payment Details Has Been Shared!");  
-        window.location.href = 'www.teachbuddies.com/paymentPage.html';  
-    })
-
-    // .then(() => { window.location.reload(); })
-    .catch(error => console.error('Error : ' , error.message))
-})
-
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(response => {
+            if (response.ok) {
+                throw new Error(`Error: ${response.statusText}`);
+            }else{
+                alert("Payment Details Has Been Shared!");
+                window.location.href = '../payment1.html'; // Ensure this path is correct
+            }
+        })
+        .catch(error => console.error('Error:', error.message));
+});
